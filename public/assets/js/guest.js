81f8bea26431c27237e5db02874f68e97c274fc9
morningmemo <grandmorning@hotmail.com>
2016-12-09 20:01:29 +0900

    add disconnect function

--- public/assets/js/guest.js
+++ public/assets/js/guest.js
@@ -2,18 +2,23 @@
 
 window.onload = function(){
   var createChannelButton = document.querySelector('#createChannel');
-    var createChannelIdInput = document.querySelector('#createChannelId');
-    var appCaller;
-    appCaller = new PlayRTC({
-      projectKey: '60ba608a-e228-4530-8711-fa38004719c1',
-      localMediaTarget: 'callerLocalVideo',
-      remoteMediaTarget: 'callerRemoteVideo'
-    });
-    appCaller.on('connectChannel', function(channelId) {
-      createChannelIdInput.value = channelId;
-    });
-    createChannelButton.addEventListener('click', function(event) {
-      event.preventDefault();
-      appCaller.createChannel();
-    }, false);
+  var createChannelIdInput = document.querySelector('#createChannelId');
+  var disconnectChannelButton = document.querySelector('#disconnectChannel');
+  var appCaller;
+  appCaller = new PlayRTC({
+    projectKey: '60ba608a-e228-4530-8711-fa38004719c1',
+    localMediaTarget: 'callerLocalVideo',
+    remoteMediaTarget: 'callerRemoteVideo'
+  });
+  appCaller.on('connectChannel', function(channelId) {
+    createChannelIdInput.value = channelId;
+  });
+  createChannelButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    appCaller.createChannel();
+  }, false);
+  disconnectChannelButton.addEventListener('click', function(event){
+    event.preventDefault();
+    appCaller.disconnectChannel();
+  })
 };
